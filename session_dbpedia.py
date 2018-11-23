11448b1c94d7f927183faa63976e0cc90c6d50fc
Şamil Güser <samil.guser@sestek.com>
2018-11-23 10:21:26 +0300

    Added Dbpedia python API code to source sontrol

---
+++ session_dbpedia.py
@@ -0,0 +1,10 @@
+import spotlight
+
+sample_txt = "President Obama on Monday will call for a new minimum tax rate for individuals making more than $1 " \
+             "million a year to ensure that they pay at least the same percentage of their earnings as other " \
+             "taxpayers, according to administration officials."
+
+a = spotlight.annotate('http://model.dbpedia-spotlight.org/en/annotate', sample_txt)
+
+print(a)
+
